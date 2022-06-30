abec29856e1d34acc7a00b593059458face0dbe8
yangming <yangming@shinemo.com>
2022-06-30 16:04:58 +0800

    feat: reference-line

--- src/components/container/canvas/index.vue
+++ src/components/container/canvas/index.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import { ref, onMounted, Ref } from 'vue'
+import { ref, onMounted, reactive, provide } from 'vue'
+import ReferenceLine from './reference-line.vue'
 
 const containerRef = ref<HTMLElement | null>(null)
 const containerRect = ref<DOMRect>()
 const emits = defineEmits(['mounted'])
+const size = reactive({
+  width: 1050,
+  height: 1500
+})
+provide('page', size)
 defineProps<{}>()
 
 onMounted(() => {
@@ -14,11 +20,18 @@ onMounted(() => {
 
 <template>
   <slot name="moving" />
-  <div class="xprocess-content_wrap">
+  <div
+    class="xprocess-content_wrap"
+    :style="{
+      width: `${size.width}px`,
+      height: `${size.height}px`
+    }"
+  >
     <div
       class="xprocess-canvas"
       :ref="v => containerRef = v"
     >
+      <ReferenceLine />
       <slot />
     </div>
   </div>
@@ -26,16 +39,16 @@ onMounted(() => {
 
 <style lang="less">
 .xprocess-content_wrap {
-  width: 1050px;
-  height: 1500px;
-  background: url(./images/canvas_bg.jpeg);
+  position: relative;
+  background: url(./images/canvas_bg.jpeg) fixed;
   padding: 1000px;
   box-sizing: content-box;
 }
 .xprocess-canvas {
   position: relative;
-  width: 1050px;
-  height: 1500px;
+  width: 100%;
+  height: 100%;
   background-color: #fff;
+  box-shadow: 0 10px 10px rgba(0, 0, 0, 0.2);
 }
 </style>
